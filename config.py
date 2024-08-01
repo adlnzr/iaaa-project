0ec0a8285d96267898a46a246af479edb6737cb7
adlnzr <nazarnia.a@gmail.com>
2024-08-01 11:16:55 +0330

    resnet_model config data_summary added

---
+++ config.py
@@ -0,0 +1,25 @@
+# config.py
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+class Device:
+    # device configuration
+    device = "cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu"
+
+class Config:
+    # hyperparameters
+    learning_rate = 0.001
+    batch_size = 32
+    num_epochs = 10
+
+    # model parameters
+    # input_size = 
+    # hidden_size = 
+    # output_size = 
+
+    # criterion
+    criterion = nn.BCEWithLogitsLoss()
+
+
